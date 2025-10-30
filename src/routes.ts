ba03bcfd3f060f001c9b2320a7b4c435d195471f
Gabriel Henrique Francisco <gabrielpavao011@gmail.com>
2025-10-30 08:36:57 -0300

    Implementa estrutura inicial da API com rotas e controle de clientes, adiciona conexão com o banco de dados e configurações necessárias

---
+++ src/routes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express"; // Importa o módulo Router do express
+import type { Request, Response } from "express"; // Importa os módulos de requisição e resposta
+import ClienteController from "./controller/ClienteController.js";
+
+const router = Router(); // cria uma instância de Router
+
+router.get("/api", (req: Request, res: Response) => {
+    res.status(200).json({ mensagem: "Olá, seja bem-vindo!" });
+});
+
+/**
+ * Endpoints (rotas) para Clientes
+ */
+// Retorna a lista com todos os clientes
+router.get("/api/clientes", ClienteController.todos);
+// Cadastra um novo cliente
+router.post("/api/clientes", ClienteController.novo);
+
+export { router }; // Exporta o roteador
